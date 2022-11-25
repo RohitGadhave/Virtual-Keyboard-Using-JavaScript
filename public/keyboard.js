85e0eb66065f20fecdb59386e39e205435a23560
Rohit Gadhave <rohitgadhave249@gmail.com>
2022-11-26 01:09:22 +0530

    email added

--- public/keyboard.js
+++ public/keyboard.js
@@ -99,6 +99,46 @@ function keyBoardInit() {
 function openKeyBoard() {
     keyBoardInit();
 }
+function keyboard() {
+    KioskBoard.run('.js-kioskboard-text', {
+        language: "en",
+        theme: "light",
+        keysArrayOfObjects: [
+            {
+                "0": "Q",
+                "1": "W",
+                "2": "E",
+                "3": "R",
+                "4": "T",
+                "5": "Y",
+                "6": "U",
+                "7": "I",
+                "8": "O",
+                "9": "P"
+            },
+            {
+                "0": "A",
+                "1": "S",
+                "2": "D",
+                "3": "F",
+                "4": "G",
+                "5": "H",
+                "6": "J",
+                "7": "K",
+                "8": "L"
+            },
+            {
+                "0": "Z",
+                "1": "X",
+                "2": "C",
+                "3": "V",
+                "4": "B",
+                "5": "N",
+                "6": "M"
+            }
+        ]
+    });
+}
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     // keyBoardInit();
